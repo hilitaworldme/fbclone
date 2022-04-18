1fcf8870da98d136f0274ab4b84c238048bc2c86
hilitaworldme <hilitaworld@proton.me>
2022-04-18 08:23:31 +0000

    build

---
+++ out/_next/static/gMFfaoR4UBCueeA7RlG3U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-38132b2d0b762d72.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
