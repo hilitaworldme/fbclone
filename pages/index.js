52e629cf5502a0044d5e5bda526f0782a7e347a0
hilitaworldme <hilitaworld@proton.me>
2022-04-18 09:52:43 +0000

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ import Head from 'next/head'
 import Header from '../components/Header'
 
 export default function Home() {
+  console.log(`prefix='${process.env.PREFIX}'`)
   return (
     <div>
       <Head>
         <title>fbclone</title>
         <meta name="description" content="a clone of fb" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={process.env.PREFIX + "/favicon.ico"} />
       </Head>
+      <Header />
       <main>
-        <Header />
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-
+{/* SideBar */}
+{/* Feed */}
+{/* Widgets */}
       </main>
     </div>
   )
